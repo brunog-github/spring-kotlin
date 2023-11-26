86ac32f78688e06e00c9a851f0aa3bdab4702c15
Bruno Gonçalves Pereira <bruno.g.p897@gmail.com>
2023-11-26 14:46:37 -0300

    first commit

---
+++ src/test/kotlin/dev/bruno/springkotlin/SpringKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.bruno.springkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
